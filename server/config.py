d039f5b4470562ec2a50f41b5aca3f1ab61b127c
Tushig Erdenebulgan <tushiggod@gmail.com>
2025-01-27 21:36:50 -0600

    Recipes App

--- server/config.py
+++ server/config.py
@@ -1,14 +1,11 @@
-# config.py
-
 import pathlib
 import os
 import dotenv
 from flask import Flask
-from flask_cors import CORS
+from flask_cors import CORS 
 from flask_marshmallow import Marshmallow
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
-import urllib.parse
 
 db = SQLAlchemy()
 mm = Marshmallow()
@@ -16,45 +13,38 @@ mm = Marshmallow()
 def create_app():
     this_app = Flask(__name__)
     this_dir = pathlib.Path(__file__)
-
-    dotenv.load_dotenv(this_dir.parent / ".flaskenv")
-
+    dotenv.load_dotenv(this_dir / pathlib.Path(".flaskenv"))
     this_app.config.from_prefixed_env()
-
-
-    db_user = os.getenv("CLOUD_SQL_USERNAME")
-    db_password = os.getenv("CLOUD_SQL_PASSWORD")
-    db_name = os.getenv("CLOUD_SQL_DATABASE")
-    connection_name = os.getenv("CLOUD_SQL_CONNECTION_NAME")
-
-    db_uri = (
-        f"postgresql+psycopg2://{db_user}:{db_password}@/"
-        f"{db_name}?host=/cloudsql/{connection_name}"
+    
+    db_file = this_dir.parent / pathlib.Path(
+        f"{this_app.config.get('FLASK_DATABASE_FILE', 'recipe_database')}.sqlite3"
     )
-    this_app.config["SQLALCHEMY_DATABASE_URI"] = db_uri
     this_app.config["SECRET_KEY"] = os.getenv("FLASK_SECRET_KEY", "default_secret")
+    this_app.config["SQLALCHEMY_DATABASE_URI"] = f"sqlite:///{db_file}"
     this_app.config["JWT_SECRET_KEY"] = os.getenv("FLASK_JWT_SECRET_KEY", "default_jwt_secret")
-    this_app.config["SQLALCHEMY_ECHO"] = bool(int(os.getenv("FLASK_SQLALCHEMY_ECHO", 0)))
-    this_app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = bool(int(os.getenv("FLASK_SQLALCHEMY_TRACK_MODIFICATIONS", 0)))
-
+    this_app.config["SQLALCHEMY_ECHO"] = os.getenv("FLASK_SQLALCHEMY_ECHO", False)
+    this_app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = os.getenv("FLASK_SQLALCHEMY_TRACK_MODIFICATIONS", False)
+    
     CORS(
         this_app,
         resources={r"/*": {"origins": [
             "http://localhost:5500",
             "http://127.0.0.1:5500",
             "http://[::]:5500"
+            "https://your-netlify-subdomain.netlify.app",
+            "https://recipesapps.com",
+            "https://www.recipesapps.com"
         ]}},
         supports_credentials=True
     )
-
+    
     db.init_app(this_app)
     mm.init_app(this_app)
-
+    
     from model import User, Recipe, SavedRecipe, FridgeItem
-
     migrate = Migrate(this_app, db)
-
-    with this_app.app_context():
-        db.create_all()
-
+    if not db_file.exists():
+        with this_app.app_context():
+            db.create_all()
+    
     return this_app
